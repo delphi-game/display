69a456e65317c08918287bdd26fa6c94133669bd
venashial <vande.shane@gmail.com>
2021-04-12 14:05:41 -0700

    added background music

---
+++ src/sound/soundManager.js
@@ -0,0 +1,25 @@
+import {Howl, Howler} from 'howler';
+Howler.volume(0.8);
+
+const music = new Howl({
+	src: [process.env.BASE_URL + 'audio/music.aac'],
+	html5: true,
+	loop: true,
+	preload: true,
+	volume: 0,
+	onloaderror: function(id, err) {
+		console.log(err)
+	},
+	onplayerror: function(id, err) {
+		console.log(err)
+	},
+});
+
+export default {
+	startMusic() {
+		const starting_point = Math.round((music.duration() * (3 /4)) * Math.random())
+		music.seek(starting_point)
+		music.play();
+		music.fade(0, 1, 6000);
+	}
+}
