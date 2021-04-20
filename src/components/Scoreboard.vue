d2d45fec3ec313a712afffc12732a84900643c33
venashial <vande.shane@gmail.com>
2021-04-19 22:54:02 -0700

    link to store + animations

--- src/components/Scoreboard.vue
+++ src/components/Scoreboard.vue
@@ -1,26 +1,65 @@
 <template>
-  <div class="leaderboard">
+  <div class="leaderboard" :style="{ fontSize: system.font_size }">
     <div class="title title-font">Score Board</div>
     <div>
-      <div class="team-score" v-for="i in 9" :key="i">
-        <div class="rank">#1</div>
-        <div class="team">Team Red</div>
-        <div class="cards-left">2 cards left</div>
-      </div>
+      <transition-group name="slide">
+        <div
+          class="team-score"
+          v-for="team in teamsArray"
+          :key="team.name"
+          :style="{ backgroundColor: teamColor(team.name) }"
+        >
+          <div class="rank">#{{ team.rank }}</div>
+          <div class="team">Team {{ capitalize(team.name) }}</div>
+          <div class="cards-left">{{ team.remaining }} cards left</div>
+        </div>
+      </transition-group>
     </div>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+import Colors from "../assets/colors.json";
+
 export default {
   props: {},
   data() {
     return {};
   },
-  computed: {
-    teams() {
-      return this.$store.state.teams;
+  methods: {
+    capitalize(s) {
+      if (typeof s !== "string") return "";
+      return s.charAt(0).toUpperCase() + s.slice(1);
     },
+    teamColor(name) {
+      return Colors[name].light;
+    },
+  },
+  computed: {
+    teamsArray() {
+      let teamsArray = [];
+      Object.keys(this.teams).forEach((name) => {
+        const teamObject = { name, ...this.teams[name] };
+        teamsArray.push(teamObject);
+      });
+
+      teamsArray.sort((a, b) => a.remaining - b.remaining);
+
+      let last_rank = 0;
+
+      teamsArray.forEach((team, index) => {
+        if (index != 0 && team.remaining == teamsArray[index - 1].remaining) {
+          teamsArray[index].rank = last_rank;
+        } else {
+          teamsArray[index].rank = last_rank + 1;
+        }
+        last_rank = teamsArray[index].rank;
+      });
+
+      return teamsArray;
+    },
+    ...mapState(["teams", "system"]),
   },
 };
 </script>
@@ -46,9 +85,10 @@ export default {
   display: grid;
   grid-template-columns: 15% 55% 30%;
   background-color: white;
-  border-radius: 0.4em;
-  padding: 0.14em 0.4em;
+  border-radius: 0.6em;
+  padding: 0.14em 0.5em;
   font-size: 75%;
+  border: 0.23em solid black;
 }
 
 .team-score > div {
@@ -57,11 +97,17 @@ export default {
 }
 
 .rank {
-  font-weight: bold;
+  font-family: "ChunkFive";
+  transform: translateY(0.12em);
+  font-size: 95%;
 }
 
 .cards-left {
   font-size: 80%;
   justify-content: flex-end;
+}
+
+.slide-move {
+  transition: transform 1s;
 }
 </style>
